6bb1df8e96e03b81f6a943683f10fd4f66d1f3d6
zap8600 <95487746+zap8600@users.noreply.github.com>
2022-09-27 13:27:56 -0500

    more ui development
    worked on the ui some more

--- Assets/Scripts/HUD.cs
+++ Assets/Scripts/HUD.cs
@@ -17,11 +17,95 @@ public class HUD : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        if (NetworkManager.singleton.networkAddress != "localhost") { inputFieldAddress.text = NetworkManager.singleton.networkAddress; }
+        
+        inputFieldAddress.onValueChanged.AddListener(delegate { ValueChangeCheck(); });
+        
         buttonHost.onClick.AddListener(ButtonHost);
+        buttonServer.onClick.AddListener(ButtonServer);
+        buttonClient.onClick.AddListener(ButtonClient);
+        buttonStop.onClick.AddListener(ButtonStop);
+        
+        SetupCanvas();
+    }
+    
+    public void ValueChangeCheck()
+    {
+        NetworkManager.singleton.networkAddress = inputFieldAddress.text;
     }
     
     public void ButtonHost()
     {
         NetworkManager.singleton.StartHost();
+        SetupCanvas();
+    }
+    
+    public void ButtonServer()
+    {
+        NetworkManager.singleton.StartServer();
+        SetupCanvas();
+    }
+
+    public void ButtonClient()
+    {
+        NetworkManager.singleton.StartClient();
+        SetupCanvas();
+    }
+
+    public void ButtonStop()
+    {
+        // stop host if host mode
+        if (NetworkServer.active && NetworkClient.isConnected)
+        {
+            NetworkManager.singleton.StopHost();
+        }
+        // stop client if client-only
+        else if (NetworkClient.isConnected)
+        {
+            NetworkManager.singleton.StopClient();
+        }
+        // stop server if server-only
+        else if (NetworkServer.active)
+        {
+            NetworkManager.singleton.StopServer();
+        }
+
+        SetupCanvas();
+    }
+
+    public void SetupCanvas()
+    {
+        // Here we will dump majority of the canvas UI that may be changed.
+
+        if (!NetworkClient.isConnected && !NetworkServer.active)
+        {
+            if (NetworkClient.active)
+            {
+                PanelStart.SetActive(false);
+                PanelStop.SetActive(true);
+                clientText.text = "Client: address=" + NetworkManager.singleton.networkAddress;
+                serverText.text = "Server: Client Only.";
+            }
+            else
+            {
+                PanelStart.SetActive(true);
+                PanelStop.SetActive(false);
+            }
+        }
+        else
+        {
+            PanelStart.SetActive(false);
+            PanelStop.SetActive(true);
+
+            // server / client status message
+            if (NetworkServer.active)
+            {
+                serverText.text = "Server: active. Transport: " + Transport.activeTransport;
+            }
+            if (NetworkClient.isConnected)
+            {
+                clientText.text = "Client: address=" + NetworkManager.singleton.networkAddress;
+            }
+        }
     }
 }
